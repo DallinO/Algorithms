86a61b8051197b717a736558b699d9d962a21c7b
Dallin Olson <dallinmolson@outlook.com>
2024-05-18 18:19:00 -0600

    Initial Commit

---
+++ AlgorithmLib/MergeSort.cs
@@ -0,0 +1,113 @@
+﻿/* CSE 381 - Merge Sort
+*  (c) BYU-Idaho - It is an honor code violation to post this
+*  file completed in a public file sharing site. S4.
+*
+*  Instructions: Implement the Merge and _Sort functions per the instructions
+*  in the comments.  Run all tests in MergeSortTest.cs to verify your code.
+*  Submit this file and the completed MergeSort.md file into Canvas.
+*/
+
+namespace AlgorithmLib;
+
+public static class MergeSort
+{
+    /* Use Merge Sort to sort a list of values in place
+        *
+        *  Inputs:
+        *     data - list of values
+        *  Outputs:
+        *     none
+        */
+    public static void Sort<T>(List<T> data) where T : IComparable<T>
+    {
+        // Start the recursive process with the whole list
+        _Sort(data, 0, data.Count - 1);
+    }
+
+    /* Recursively use merge sort to sort a sublist
+        * defined by first and last.
+        * 
+        *  Inputs:
+        *     data - list of values
+        *     first - the start of the sublist
+        *     last - the end of the sublist
+        *  Outputs:
+        *     None
+        */
+    private static void _Sort<T>(List<T> data, int first, int last) where T : IComparable<T>
+    {
+        if (first < last)
+        {
+            int mid = (first + last) / 2;
+            _Sort(data, first, mid); // Sort the left half
+            _Sort(data, mid + 1, last); // Sort the right half
+            Merge(data, first, mid, last); // Merge the sorted halves
+        }
+    }
+
+    /* Merge two sorted lists which are adjacent to each other back into
+        * the same list.
+        *
+        *  Inputs:
+        *     data - list of values
+        *     first - the start of the first sorted sublist
+        *     mid - the end of the first sorted sublist (second sublist starts after)
+        *     last - the end of the second sorted sublist
+        *  Outputs:
+        *     None
+        */
+    private static void Merge<T>(List<T> data, int first, int mid, int last) where T : IComparable<T>
+    {
+        int leftSize = mid - first + 1;
+        int rightSize = last - mid;
+
+        T[] leftArray = new T[leftSize];
+        T[] rightArray = new T[rightSize];
+
+        // Copy data to temporary arrays
+        for (int i = 0; i < leftSize; i++)
+        {
+            leftArray[i] = data[first + i];
+        }
+        for (int j = 0; j < rightSize; j++)
+        {
+            rightArray[j] = data[mid + 1 + j];
+        }
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int mergeIndex = first;
+
+        // Merge the two arrays back into data
+        while (leftIndex < leftSize && rightIndex < rightSize)
+        {
+            if (leftArray[leftIndex].CompareTo(rightArray[rightIndex]) <= 0)
+            {
+                data[mergeIndex] = leftArray[leftIndex];
+                leftIndex++;
+            }
+            else
+            {
+                data[mergeIndex] = rightArray[rightIndex];
+                rightIndex++;
+            }
+            mergeIndex++;
+        }
+
+        // Copy remaining elements of leftArray, if any
+        while (leftIndex < leftSize)
+        {
+            data[mergeIndex] = leftArray[leftIndex];
+            leftIndex++;
+            mergeIndex++;
+        }
+
+        // Copy remaining elements of rightArray, if any
+        while (rightIndex < rightSize)
+        {
+            data[mergeIndex] = rightArray[rightIndex];
+            rightIndex++;
+            mergeIndex++;
+        }
+    }
+}
---
+++ AlgorithmLib/QuickSort.cs
@@ -0,0 +1,86 @@
+﻿/*  CSE 381 - Quick Sort
+ *  (c) BYU-Idaho - It is an honor code violation to post this
+ *  file completed in a public file sharing site. S4.
+ *
+ *  Instructions: Implement the Partition and _Sort functions per the instructions
+ *  in the comments.  Run all tests in QuickSortTest.cs to verify your code.
+ *  Submit this file and the completed QuickSort.md file into Canvas.
+ */
+namespace AlgorithmLib;
+
+public static class QuickSort
+{
+
+
+    /* Use Quick Sort to sort a list of values in place
+     *
+     *  Inputs:
+     *     data - list of values
+     *  Outputs:
+     *     none
+     */
+    public static void Sort<T>(List<T> data) where T : IComparable<T>
+    {
+        // Start the recursion with the entire list
+        _Sort(data, 0, data.Count-1);
+    }
+
+    /* Recursively use quick sort to sort a sublist
+     * defined by first and last.
+     *
+     *  Inputs:
+     *     data - list of values
+     *     first - the start of the sublist
+     *     last - the end of the sublist
+     *  Outputs:
+     *     None
+     */
+    public static void _Sort<T>(List<T> data, int first, int last) where T : IComparable<T>
+    {
+        if (first < last)
+        {
+            int pivotIndex = Partition(data, first, last); // Partition the list and get pivot index
+            _Sort(data, first, pivotIndex - 1); // Recursively sort the left partition
+            _Sort(data, pivotIndex + 1, last); // Recursively sort the right partition
+        }
+    }
+
+
+    /* Partition a sublist by finding where a pivot belongs when sorted.  All
+     * values less or equal to the pivot must be on the left hand side and
+     * all values greater must be on the right hand size of the pivot.
+     * In this implementation, do not select a random pivot.  Select the
+     * last value in the sublist to always be your pivot.
+     *
+     *  Inputs:
+     *     data - list of values
+     *     first - the start of the sublist
+     *     last - the end of the sublist
+     *  Outputs:
+     *     The index of where the pivot was moved
+     */
+    public static int Partition<T>(List<T> data, int first, int last) where T : IComparable<T>
+    {
+        T pivot = data[last];
+        int i = first - 1;
+
+        for (int j = first; j < last; j++)
+        {
+            
+            if (data[j].CompareTo(pivot) <= 0)
+            {
+                i++;
+                T temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+            }
+        }
+
+        T temp2 = data[i + 1];
+        data[i + 1] = data[last];
+        data[last] = temp2;
+
+        return i + 1; 
+    }
+
+}
